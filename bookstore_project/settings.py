38b33ba08b519b688d635c9dab96fe0a28d38307
Tome Cirun <cirun@live.com>
2021-01-29 20:59:27 +0100

    ch5

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
@@ -127,3 +128,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'  # new
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
