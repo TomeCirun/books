c4d80b91512bcdb04a44670439931832fa04b9d4
TomeCirun <72216462+TomeCirun@users.noreply.github.com>
2021-02-17 13:32:40 +0100

    Update settings.py

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'jasamikoj',
+        'PASSWORD': 'pass',
         'HOST': 'db',
         'PORT': 5432
     }
